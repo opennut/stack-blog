d9de6257bfeae16409d5edf721c4da5ef5d6c011
Wario Solis <mario.solis@tharix.com>
2015-09-25 18:02:07 -0600

    gitignore

--- stackblog/stackblog/models.py
+++ stackblog/stackblog/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.sites.models import Site
 
-class StackBlog(models.Model):
+class StackBlog(object):
 	"""docstring for StackBlog"""
 	site = models.OneToOneField(Site, verbose_name="Site", blank=False, null=False)
 	
@@ -9,5 +9,3 @@ class StackBlog(models.Model):
 		super(StackBlog, self).__init__()
 		self.arg = arg
 		
-
-class Language()
